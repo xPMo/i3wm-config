4a9a925e8bdac153796ededc136e6949cb240713
xPMo <pmo.pgm@gmail.com>
2016-08-02 02:52:32 -0500

    Preliminary support for scripted installation
    Replaces '~/.i3' in all config.d files with '$installDir', which is
    replaced on install by the new script 'firstInstall.sh' More changes to
    come to the firstInstall script

---
+++ firstInstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# installDir is a string for the sake of unexpanded ~ in config files
+installDir="~/.i3"
+dirDest=~/.i3
+dirSource=$PWD
+echo $dirDest
+if [ ! -d $dirDest ]; then
+   mkdir $dirDest
+fi
+cp -r config.d $dirDest
+cp -r scripts $dirDest
+for file in $(find "$dirDest" -type f)
+do
+   sed -i "s@\$installDir@$(echo $installDir | sed 's@\.@\\.@g')@g" $file
+done
