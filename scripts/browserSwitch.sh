944707e2f0c765091b69cd826811458c288c0d75
xPMo <pmo.pgm@gmail.com>
2016-08-06 00:53:52 -0500

    Combo scripts now take workspace arguments

--- scripts/browserSwitch.sh
+++ scripts/browserSwitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ $(i3-msg [class="irefox"] focus | grep -ic "false") -gt 0 ]; then
-	i3-msg "workspace 12:WWW, exec firefox"
+	i3-msg "workspace $1, exec firefox"
 	echo hitrue
 fi
--- scripts/termSwitch.sh
+++ scripts/termSwitch.sh
@@ -2,10 +2,10 @@
 #set -euo pipefail
 #IFS=$'\n\t'
 
-if i3-msg -t get_workspaces | grep -q '"11:~$","visible":t'; then
-	i3-msg "workspace 11:~$; exec i3-sensible-terminal"
+if i3-msg -t get_workspaces | grep -q "\"$1\",\"visible\":t"; then
+	i3-msg "workspace $1; exec i3-sensible-terminal"
 else
-	i3-msg "workspace 11:~$"
+	i3-msg "workspace $1"
 	if ! xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS | grep -q "term"; then
 		i3-msg "exec i3-sensible-terminal"
 	fi
