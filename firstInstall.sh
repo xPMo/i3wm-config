dd4c717d31795e1867da8e5a06d13747b5abee82
xPMo <pmo.pgm@gmail.com>
2016-08-06 01:02:02 -0500

    Simplified install script\n\nUser now sets string for source file

--- firstInstall.sh
+++ firstInstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# installDir is a string for the sake of unexpanded ~ in config files
-installDir="~/.i3"
+escapedDest="~/\.i3"
 dirDest=~/.i3
+
 dirSource=$PWD
+
 echo $dirDest
 if [ ! -d $dirDest ]; then
    mkdir $dirDest
@@ -13,7 +14,7 @@ cp -r scripts $dirDest
 # Set install directory for config updating and scripts
 for file in $(find "$dirDest" -type f)
 do
-   sed -i "s@\$installDir@$(echo $installDir | sed 's@\.@\\.@g')@g" $file
+   sed -i "s@\$installDir@$escapedDest@g" $file
 done
 
 # Automatically set primary monitor and secondary monitor, if connected
