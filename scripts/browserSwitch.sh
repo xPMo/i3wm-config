2047b3770a0f153fe3e9c798be90e74abfb09c37
xPMo <pmo.pgm@gmail.com>
2016-08-06 00:36:26 -0500

    browserSwitch script now has expected behaviour

--- scripts/browserSwitch.sh
+++ scripts/browserSwitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if i3-msg [class="irefox"] focus | grep -q ERROR; then
+if [ $(i3-msg [class="irefox"] focus | grep -ic "false") -gt 0 ]; then
 	i3-msg "workspace 12:WWW, exec firefox"
-	echo true
+	echo hitrue
 fi
