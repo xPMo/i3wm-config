6eda54f1431538cf8336fd67d813312b4fbe2cf9
xPMo <pmo.pgm@gmail.com>
2016-08-06 16:47:13 -0500

    switchLaunch works as intended with the new arguments

--- scripts/switchLaunch.sh
+++ scripts/switchLaunch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #set -euo pipefail
 #IFS=$'\n\t'
-
-# Arguments: program, program's WM_CLASS lookup, workspace
-
-if i3-msg -t get_workspaces | grep -q "\"$2\",\"visible\":t"; then
+#Arguments: program, program's WM_CLASS lookup, workspace
+if [ $(i3-msg -t get_workspaces | grep -ic "\"$3\",\"visible\":t") -gt 0 ] ; then
 	i3-msg "workspace $3; exec $1"
 else
 	i3-msg "workspace $3"
-	if ! xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS | grep -q "$2"; then
+	if [ $(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS | grep -c "$2") -eq 0 ]; then
 		i3-msg "exec $1"
 	fi
 fi
