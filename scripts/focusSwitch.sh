f93d3ed91e7bfd6a1c71cca4a076ae97379cea51
xPMo <pmo.pgm@gmail.com>
2016-08-16 22:13:03 -0500

    SC Keyboard and combo fixes
    Steam Controller Keyboard is now unfocused after 0.2 seconds, and the
    combo runs now correctly parse the i3 error messages

--- scripts/focusSwitch.sh
+++ scripts/focusSwitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-if [ $(i3-msg [class="$2"] focus | grep -ic "false") -gt 0 ]; then
+if [ $(i3-msg [class="$2"] focus 2>&1 | grep -ic "ERROR") -gt 0  ]; then
 	i3-msg "workspace $3, exec $1"
 fi
