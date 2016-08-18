f350bfc130a4ffe2b567d10485b80773181eddc8
xPMo <pmo.pgm@gmail.com>
2016-08-18 03:46:05 -0500

    i3status config files are now installed as well

--- firstInstall.sh
+++ firstInstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 escapedDest="~/\.i3"
 dirDest=~/.i3
+escapedStatusDest="~/.config/i3status"
+statusDirDest=~/.config/i3status
+
 browser="firefox"
 terminal="xfce4-terminal"
 
@@ -16,17 +19,20 @@ if [ ! -d $dirDest ]; then
 fi
 cp -r config.d $dirDest
 cp -r scripts $dirDest
+cp -r i3status $statusDirDest
 
 # Set install directory for config updating and scripts
 for file in $(find "$dirDest" -type f)
 do
    sed -i "s@\$installDir@$escapedDest@g" $file
+   sed -i "s@\$statusDir@$escapedStatusDest@g" $file
 done
 
 # Automatically set primary monitor and secondary monitor, if connected
 sed -i 0,/\$MONITOR/{s/\$MONITOR/$(xrandr | egrep .+primary | egrep -o '^(\w|-|_)+')/} $dirDest/config.d/2-binds
 
 sed -i 0,/\$MONITOR/{s/\$MONITOR/$(xrandr | grep \ connected\ [^p] | egrep -o "^(\w|-|_)+")/} $dirDest/config.d/2-binds
+
 sed -i "s/\$BROWSERCLASS/$browserClass/" $dirDest/config.d/2-binds
 sed -i "s/\$TERMCLASS/$termClass/" $dirDest/config.d/2-binds
 sed -i "s/\$BROWSER/$browser/" $dirDest/config.d/2-binds
