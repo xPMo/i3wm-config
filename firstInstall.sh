0c633d0efae39b9f5fbd6ee2c0b131980735ffe9
xPMo <pmo.pgm@gmail.com>
2016-08-21 10:56:36 -0500

    focusSwitch for mailClient, default is Icedove

--- firstInstall.sh
+++ firstInstall.sh
@@ -3,15 +3,15 @@ escapedDest="~/\.i3"
 dirDest=~/.i3
 escapedStatusDest="~/.config/i3status"
 statusDirDest=~/.config/i3status
+dirSource=$PWD
 
 browser="firefox"
 terminal="xfce4-terminal"
-
+mail="icedove"
 # Some subtring for WM_CLASS lookup
 browserClass="irefox"
 termClass="erm"
-
-dirSource=$PWD
+mailClass="Icedove"
 
 echo $dirDest
 if [ ! -d $dirDest ]; then
@@ -34,9 +34,11 @@ sed -i 0,/\$MONITOR/{s/\$MONITOR/$(xrandr | egrep .+primary | egrep -o '^(\w|-|_
 sed -i 0,/\$MONITOR/{s/\$MONITOR/$(xrandr | grep \ connected\ [^p] | egrep -o "^(\w|-|_)+")/} $dirDest/config.d/2-binds
 
 sed -i "s/\$BROWSERCLASS/$browserClass/" $dirDest/config.d/2-binds
+sed -i "s/\$MAILCLASS/$mailClass/" $dirDest/config.d/2-binds
 sed -i "s/\$TERMCLASS/$termClass/" $dirDest/config.d/2-binds
 sed -i "s/\$BROWSER/$browser/" $dirDest/config.d/2-binds
 sed -i "s/\$TERM/$terminal/" $dirDest/config.d/2-binds
+sed -i "s/\$MAIL/$mail/" $dirDest/config.d/2-binds
 
 if [ ! -f $dirDest/config ]; then
    touch $dirDest/config
