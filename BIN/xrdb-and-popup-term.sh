6fdd81cf3104935a6af71c02bf43219ab724c156
xPMo <PMo_@vivaldi.net>
2018-02-28 23:42:19 -0600

    popup_term is darker and supports compton transparency

---
+++ BIN/xrdb-and-popup-term.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+DISPLAY=:0
+xrdb ~/.Xresources
+i3-msg "[title=^popup_term$]" focus 2>&1 | grep -q "ERROR" \
+	&& exec i3-msg exec -- "--no-startup-id DISABLE_AUTO_TITLE=true exec urxvt -title 'popup_term' -bg '[80]#000c0f'"
