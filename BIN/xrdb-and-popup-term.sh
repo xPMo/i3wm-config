40d4ebbe4601b79ee884f0856f7aa908270a5da9
xPMo <PMo_@vivaldi.net>
2018-03-01 18:11:05 -0600

    missplling fixed

--- BIN/xrdb-and-popup-term.sh
+++ BIN/xrdb-and-popup-term.sh
@@ -2,4 +2,4 @@
 DISPLAY=:0
 xrdb -merge ~/.Xresources
 i3-msg "[title=^popup_term$]" focus 2>&1 | grep -q "ERROR" \
-	&& exec i3-msg exec -- "--no-startup-id DISABLE_AUTO_TITLE=true exec urxvt -title 'popup_term' -bg '[80]#000c0f'"
+	&& exec i3-msg exec -- "--no-startup-id DISABLE_AUTO_TITLE=true exec urxvt -title 'popup_term' -bg '[80]#000c0f' -fadecolor '[60]#000000'"
