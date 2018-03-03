934a0091d3ce763d1a1e197cd05718f4a2de8b57
xPMo <PMo_@vivaldi.net>
2018-03-02 21:16:26 -0600

    xrdb-and-popup-term

--- BIN/xrdb-and-popup-term.sh
+++ BIN/xrdb-and-popup-term.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 DISPLAY=:0
-xrdb -merge ~/.Xresources
-i3-msg "[title=^popup_term$]" focus 2>&1 | grep -q "ERROR" \
-	&& exec i3-msg exec -- "--no-startup-id DISABLE_AUTO_TITLE=true exec urxvt -title 'popup_term' -bg '[80]#000c0f' -fadecolor '[60]#000000'"
+function usage {
+	echo "$0 [ options ]
+
+	-n        	force launch new instance
+	-x        	merge ~/.Xresources first
+	-t title  	set title of window
+	-o opacity	set opacity [0,100]
+	-f fade   	set fade percentage [0,100]"
+	exit 1
+}
+function nterm {
+	i3-msg exec -- "--no-startup-id DISABLE_AUTO_TITLE=true \
+		exec urxvt \
+		-title '${title:-popup_term}' \
+		-bg '[${opacity:-70}]#00080a' \
+		-fadecolor '[60]#000000' \
+		-fade ${fade:-30}"
+}
+cond() {
+	i3-msg "[title=^${title:-popup_term}$]" focus 2>&1 | grep -q "ERROR"
+}
+while getopts "hnxt:o:f:" opt; do
+	case ${opt} in
+		f )
+			if (( 0 <= OPTARG && OPTARG <= 100 )); then
+				fade="${OPTARG}"
+			else usage
+			fi ;;
+		n ) cond(){ :; } ;;
+		o )
+			if (( 0 <= OPTARG && OPTARG <= 100 )); then
+				opacity="$(( OPTARG ))"
+			else usage
+			fi ;;
+		t ) title="${OPTARG}" ;;
+		x ) xrdb -merge ~/.Xresources ;;
+		h ) usage ;;
+	esac
+done
+cond && nterm
