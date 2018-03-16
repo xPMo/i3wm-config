c92952059989f25214aa8deb617bbb5b54769459
xPMo <PMo_@vivaldi.net>
2018-03-16 03:37:12 -0500

    Better link-if-exists function

--- init.sh
+++ init.sh
@@ -4,11 +4,16 @@ set -e
 
 #link-if-exists
 function lie {
-	[ -L "$2" ] && return
-	if [ -f "$2" ]; then
+	# refer to the same file
+	[[ $(realpath "$1") = $(realpath "$2") ]] && return
+	diff "$1" "$2" -q 2>/dev/null && return
+
+	if [ -e "$2" ]; then
+		# path exists already
 		echo "$2 exists and is not a symlink. Linking to $2.new"
 		ln -s "$1" "$2.new"
 	else
+		# create link
 		ln -s "$1" "$2"
 	fi
 }
