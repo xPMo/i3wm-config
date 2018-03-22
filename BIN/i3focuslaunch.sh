4ea27a0366bcbdbd6661f1ccc353a50c73f5aff3
xPMo <PMo_@vivaldi.net>
2018-03-22 13:16:34 -0500

    use i3-msg exec instead of exec

--- BIN/i3focuslaunch.sh
+++ BIN/i3focuslaunch.sh
@@ -7,7 +7,6 @@ IFS="
 # (?i) for case-insensitivity
 class=${2:-(?i)$1}
 # focus $class if possible, else exec $1
-echo $class
 i3-msg "[class=^$class]" focus 2>&1 | grep -q "ERROR" \
 	&& i3-msg "[title=^$class]" focus 2>&1 | grep -q "ERROR" \
-	&& exec $1
+	&& i3-msg exec exec "$1"
