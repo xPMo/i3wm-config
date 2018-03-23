1873a158c5e6af43c289c6e8776d1ce6a7c3af73
xPMo <PMo_@vivaldi.net>
2018-03-22 19:06:40 -0500

    scrot ⇒ maim

--- BIN/i3slopsize.sh
+++ BIN/i3slopsize.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-eval $(slop -b 3 -c 0.96,0.5,0.09 -t 0 -f "X=%x Y=%y W=%w H=%h")
-i3 floating enable && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
+eval $(slop -q -b 4 -c 0.03,0.21,0.26,0.5 -t 0 -f "X=%x Y=%y W=%w H=%h")
+i3-msg floating enable && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
--- BIN/scrotlock.sh
+++ BIN/scrotlock.sh
@@ -5,10 +5,15 @@
 IFS="
 " # but it does allow this
 
-img=/tmp/lock.png
-if [ ! -f $img ]; then
-	touch $img
+img=$(mktemp --suffix=.png)
+if command -v maim > /dev/null; then
+	maim $img
+elif command -v scrot > /dev/null; then
+	scrot $img
+else
+	import -window root $img
 fi
-scrot $img
+# convert inplace
 convert $img -scale 20x20% -modulate 100,50 -scale 500x500% $img
-exec i3lock -i $img
+i3lock -i $img
+rm $img
