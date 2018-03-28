080bbd5052d60f67996581bc9baa9bbc749339a9
xPMo <PMo_@vivaldi.net>
2018-03-28 01:16:54 -0500

    add i3exit script from Manjaro

--- BIN/scrotlock.sh
+++ BIN/scrotlock.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env sh
-
-# POSIX doesn't allow this (yet)
-#IFS=$'\n'
 IFS="
-" # but it does allow this
-
+"
 img=$(mktemp --suffix=.png)
-if command -v maim > /dev/null; then
-	maim $img
-elif command -v scrot > /dev/null; then
-	scrot $img
-else
-	import -window root $img
-fi
+trap '{ rm $img; exit $?; }' INT
+import -window root $img
 # convert inplace
 convert $img -scale 20x20% -modulate 100,50 -scale 500x500% $img
 i3lock -i $img
