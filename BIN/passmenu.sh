d3e40873ff46c41c951a779c484da24c48b9ea46
xPMo <PMo_@vivaldi.net>
2018-04-25 15:21:29 -0500

    Add passmenu fork

---
+++ BIN/passmenu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+# alternative to standard passmenu:
+# * replaces dmenu with rofi
+# * uses POSIX sh instead of bashisms
+IFS='
+	'
+set -e
+
+typeit=0
+if [ $1 = "--type" ]; then
+	typeit=1
+	shift
+fi
+
+prefix=${PASSWORD_STORE_DIR:-~/.password-store}
+
+password=$(
+	find $prefix -iname '*.gpg' |
+		sed -e "s:^${prefix}/::" -e "s/....$//" |
+		rofi -dmenu -i "$@"
+)
+
+[ -n ${password:-} ] || exit
+
+if [ $typeit -eq 0 ]; then
+	pass show -c $password 2>/dev/null
+else
+	pass show $password | tr -d '\n' |
+		xdotool type --clearmodifiers --file -
+fi
