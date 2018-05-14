f67c7744b83a47693d1ee59415075e8e089f2e22
xPMo <PMo_@vivaldi.net>
2018-05-14 13:38:12 -0500

    Fix that ugly help text (sorry Manjaro)

--- BIN/i3tool.sh
+++ BIN/i3tool.sh
@@ -16,29 +16,50 @@ lock() {
 }
 
 case "$1" in
-	lock) lock ;;
-	logout) i3-msg exit ;;
-	switch_user) dm-tool switch-to-greeter ;;
-	suspend) lock && $logind suspend ;;
-	hybrid-sleep) lock && $logind hybrid-sleep ;;
-	hibernate) lock && $logind hibernate ;;
-	reboot) $logind reboot ;;
-	shutdown) $logind poweroff ;;
-	run)
+	loc*) # lock
+		lock ;;
+	log*|e*|x*) # logout
+		i3-msg exit ;;
+	sw* ) # switch user
+		dm-tool switch-to-greeter ;;
+	sus*) # suspend to RAM
+		lock && $logind suspend ;;
+	hi*) # suspend to disk
+		lock && $logind hibernate ;;
+	hy*) # hybrid sleep (RAM and disk)
+		lock && $logind hybrid-sleep ;;
+	re*) # reboot
+		$logind reboot ;;
+	sh*|po*) # shutdown
+		$logind poweroff ;;
+	run|m*)
 		shift
 		[ -z ${XDG_SESSION_ID:-} ] && XDG_RUNTIME_DIR="/run/user/$(id -u)"
 		i3sock=$(find ${XDG_RUNTIME_DIR}/i3 -type s)
 		exec i3-msg -s $i3sock "exec '$@'"
 		;;
 	*)
-		cat >&2 <<- EOF
+		cat >&2 << EOF
+$(basename $0) [ action ]
 
-== ! i3tool: missing or invalid argument ! ==
-Supported arguments:
-lock | logout | switch_user | suspend | hibernate | hybrid-sleep | reboot | shutdown
+Actions:
+	lock                 locks the session
+	logout | exit | x    exits i3
+	switch-user          switches user
+	suspend              locks the session and suspends RAM
+	hibernate            locks the session and suspends to disk
+	hybrid-sleep         locks the session and suspends both RAM and disk
+	reboot               reboots the machine
+	shutdown | poweroff  powers off the machine
+	run [prog [args ..]] runs the given program with its args via \`i3-msg exec\`
+	msg [prog [args ..]] runs the given program with its args via \`i3-msg exec\`
+
+This program matches the action on just the first few characters, so 
+be aware that both "log" and "logarithms" are matched by "logout".
+
 EOF
 
-		exit 2
+	exit 2
 esac
 
 exit 0
