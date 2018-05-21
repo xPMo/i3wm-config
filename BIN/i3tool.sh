e6b5b7619ada13c5e615836852007a4858523b3a
xPMo <PMo_@vivaldi.net>
2018-05-21 12:55:21 -0500

    Add abbreviations to i3tool

--- BIN/i3tool.sh
+++ BIN/i3tool.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 IFS="
 "
+set -e
 
 # with openrc use loginctl
 [ $(cat /proc/1/comm) = "systemd" ] && logind=systemctl || logind=loginctl
@@ -8,58 +9,59 @@ IFS="
 lock() {
 	img=$(mktemp --suffix=.png)
 	trap '{ rm $img; exit $?; }' INT
-	import -window root $img
+	maim $img 2>/dev/null || import -window root $img
 	# convert inplace
 	convert $img -scale 20x20% -modulate 100,50 -scale 500x500% $img
 	i3lock -i $img
 	rm $img
 }
 
-case "$1" in
-	loc*) # lock
+a=$(echo $1 | tr '[A-Z]' '[a-z]')
+case $a in
+	loc* ) # lock
 		lock ;;
-	log*|e*|x*) # logout
+	log*|e*|x* ) # logout
 		i3-msg exit ;;
-	sw* ) # switch user
+	sw*|su ) # switch user
 		dm-tool switch-to-greeter ;;
-	sus*) # suspend to RAM
+	sus* ) # suspend to RAM
 		lock && $logind suspend ;;
-	hi*) # suspend to disk
+	hi* ) # suspend to disk
 		lock && $logind hibernate ;;
-	hy*) # hybrid sleep (RAM and disk)
+	hy*|hs ) # hybrid sleep (RAM and disk)
 		lock && $logind hybrid-sleep ;;
-	re*) # reboot
+	re*|rb ) # reboot
 		$logind reboot ;;
-	sh*|po*) # shutdown
+	sh*|po*|sd ) # shutdown
 		$logind poweroff ;;
-	run|m*)
+	run|m* )
 		shift
 		[ -z ${XDG_SESSION_ID:-} ] && XDG_RUNTIME_DIR="/run/user/$(id -u)"
-		i3sock=$(find ${XDG_RUNTIME_DIR}/i3 -type s)
-		exec i3-msg -s $i3sock "exec '$@'"
+		exec i3-msg -s $(find ${XDG_RUNTIME_DIR}/i3 -type s) "exec '$@'"
 		;;
-	*)
+	* )
 		cat >&2 << EOF
-$(basename $0) [ action ]
+Usage: $(basename $0) [ action ]
 
 Actions:
-	lock                 locks the session
-	logout | exit | x    exits i3
-	switch-user          switches user
-	suspend              locks the session and suspends RAM
-	hibernate            locks the session and suspends to disk
-	hybrid-sleep         locks the session and suspends both RAM and disk
-	reboot               reboots the machine
-	shutdown | poweroff  powers off the machine
-	run [prog [args ..]] runs the given program with its args via \`i3-msg exec\`
-	msg [prog [args ..]] runs the given program with its args via \`i3-msg exec\`
+	lock                      locks the session
+	logout | exit | x         exits i3
+	switch-user | su          switches user
+	suspend                   locks the session and suspends RAM
+	hibernate                 locks the session and suspends to disk
+	hybrid-sleep | hs         locks the session and suspends both RAM and disk
+	reboot | rb               reboots the machine
+	shutdown | poweroff | sd  powers off the machine
+	run [prog [args ..]]      runs the given program with its args
+	                          via \`i3-msg exec\`
+	msg [prog [args ..]]      runs the given program with its args
+	                          via \`i3-msg exec\`
 
-This program matches the action on just the first few characters, so 
-be aware that both "log" and "logarithms" are matched by "logout", for
-example.
+This program matches the action on just the first few characters
+(unless using an abbreviation). So be aware that both "log" and
+"logarithms" are matched by "logout", for example.
 
 EOF
-
 	exit 2
 esac
 
