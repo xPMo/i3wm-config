88bca58e1d453c95281ab4f954474af7f928c847
xPMo <PMo_@vivaldi.net>
2018-11-05 18:46:19 -0600

    TODO: use jq instead of `grep`ing for error

--- BIN/i3focuslaunch.sh
+++ BIN/i3focuslaunch.sh
@@ -7,6 +7,7 @@ IFS="
 # (?i) for case-insensitivity
 class=${2:-(?i)$1}
 # focus $class if possible, else exec $1
+# TODO: i3-msg -t get_tree | jq "recurse(.nodes[])|select(.window_properties.class==\"$class\").id"
 i3-msg "[class=^$class]" focus 2>&1 | grep -q "ERROR" \
 	&& i3-msg "[title=^$class]" focus 2>&1 | grep -q "ERROR" \
 	&& i3-msg exec exec "$1"
