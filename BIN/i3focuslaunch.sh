71129c5c10d9e5b329dcc675ec0be395f8bf2d79
xPMo <PMo_@vivaldi.net>
2018-11-08 13:55:46 -0600

    use i3-msg + jq (applicable to sway as well)

--- BIN/i3focuslaunch.sh
+++ BIN/i3focuslaunch.sh
@@ -4,10 +4,14 @@ IFS="
 #Arguments: program, program's WM_CLASS lookup
 
 # class in $2, else try program name ($1)
+class=${2:-$1}
 # (?i) for case-insensitivity
-class=${2:-(?i)$1}
-# focus $class if possible, else exec $1
-# TODO: i3-msg -t get_tree | jq "recurse(.nodes[])|select(.window_properties.class==\"$class\").id"
-i3-msg "[class=^$class]" focus 2>&1 | grep -q "ERROR" \
-	&& i3-msg "[title=^$class]" focus 2>&1 | grep -q "ERROR" \
-	&& i3-msg exec exec "$1"
+con_id=$( i3-msg -t get_tree | jq 'recurse(.nodes[]) |
+	select(.window_properties|type=="object") | select(
+		(.window_properties.class | contains("'$class'"))
+		or (.window_properties.instance | contains("'$class'"))
+	).id
+' | head -n1 )
+if [ -n "${con_id:-}" ]; then
+	i3-msg "[con_id=${con_id%% }]" focus
+else i3-msg exec exec "$1"; fi
