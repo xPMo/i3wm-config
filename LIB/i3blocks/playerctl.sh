f84d2cde0b8a2a121deaf6c1f01a57a3359b0ff1
xPMo <PMo_@vivaldi.net>
2018-11-18 04:54:32 -0600

    remove excess {} arround inner loop

--- LIB/i3blocks/playerctl.sh
+++ LIB/i3blocks/playerctl.sh
@@ -5,19 +5,18 @@ IFS=$'\n\t'
 while true; do
 	# requires playerctl>=2.0
 	playerctl --follow metadata --format \
-		$'{{status}}\t{{artist}} - {{title}} {{duration(position)}}|{{duration(mpris:length)}}' | {
-		while read status line; do
-			# escape [&<>] for pango formatting
-			line=${line/&/&amp;}
-			line=${line/>/&gt;}
-			line=${line/</&lt;}
-			case $status in
-				Paused) echo "<span foreground=\"#cccc00\" size=\"smaller\">$line</span>" ;;
-				Playing) echo "<small>$line</small>" ;;
-				Stopped) echo '<span foreground="#073642">⏹</span>' ;;
-			esac
-		done
-	}
+		$'{{status}}\t{{artist}} - {{title}} {{duration(position)}}|{{duration(mpris:length)}}' |
+	while read status line; do
+		# escape [&<>] for pango formatting
+		line=${line/&/&amp;}
+		line=${line/>/&gt;}
+		line=${line/</&lt;}
+		case $status in
+			Paused) echo "<span foreground=\"#cccc00\" size=\"smaller\">$line</span>" ;;
+			Playing) echo "<small>$line</small>" ;;
+			Stopped) echo '<span foreground="#073642">⏹</span>' ;;
+		esac
+	done
 	# no current players
 	echo '<span foreground=#dc322f>⏹</span>'
 	sleep 15
