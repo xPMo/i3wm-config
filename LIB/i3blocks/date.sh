ca4015f834ed5e8000f3f4078fd65b71ca9f7ca4
xPMo <PMo_@vivaldi.net>
2019-01-07 06:34:37 -0600

    make shellcheck happier

--- LIB/i3blocks/date.sh
+++ LIB/i3blocks/date.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
-IFS='
-	'
-d=$(dirname $0)
-$d/_time_loop &
+d="$(dirname $0)"
+"$d/_time_loop" &
 while read button; do
-	case $button in
+	case "$button" in
 		3) sys-notif date;;
 	esac
 done
