b839e649ccc85838849a9008b54dcd1b35b34c1d
xPMo <PMo_@vivaldi.net>
2019-01-09 14:37:09 -0600

    add script exec_wrapper.sh

---
+++ LIB/i3/exec_wrapper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+"$@"
+status="$?"
+case "$status" in
+	0) exit ;;
+	127) msg="Command not found: $*" ;;
+	*) msg="Status $status: $*" ;;
+esac
+
+notify-send "$1 failed with status $status" "$msg"
+echo "$msg" >> "$HOME/log"
