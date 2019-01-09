236e7d2779eccd9b87591305529556945924c4ed
xPMo <PMo_@vivaldi.net>
2019-01-09 01:46:40 -0600

    use awk instead of grep | cut

--- BIN/i3volumectl.sh
+++ BIN/i3volumectl.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 IFS="
-" # IFS=$'\n'
-SINK=$(pacmd dump | grep lt-si | cut --delimiter=' ' -f 2)
+"
+SINK="$(pacmd dump | awk '/set-default-si/{print $2}')"
 # The first parameter sets the step to change the volume by (and units to display)
 # Expected in the form "(+|-)([0-9]+)%"
-if [ -z ${1:-} ]; then
-	pactl set-sink-mute $SINK toggle
+if [ -z "${1:-}" ]; then
+	pactl set-sink-mute "$SINK" toggle
 else
-	pactl set-sink-mute $SINK false
+	pactl set-sink-mute "$SINK" false
 	# This may be in in % or dB (eg. 5% or 3dB)
-	pactl set-sink-volume $SINK ${1}
+	pactl set-sink-volume "$SINK" "$1"
 fi
 exec pkill -RTMIN+10 i3blocks
